a122c2ef91c6facede8625b9b3bc31f424c61786
NataliaDragun <natkarusak1994@gmail.com>
2023-03-26 00:31:49 +0300

    added order page

---
+++ src/js/scroll_up.js
@@ -0,0 +1,25 @@
+const btnUp = {
+  el: document.querySelector('.btn__up'),
+  show() {
+    this.el.classList.remove('btn__up_hide');
+  },
+  hide() {
+    this.el.classList.add('btn__up_hide');
+  },
+  addEventListener() {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY;
+      scrollY > 100 ? this.show() : this.hide()
+    })
+
+    document.querySelector('.btn__up').onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+
+btnUp.addEventListener();
